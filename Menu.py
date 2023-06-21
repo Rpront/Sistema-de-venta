5636f47c2f73d66dc53075d353fe0d2b1b9090e7
Sebastian David Pinzon <spinzonzambrano@gmail.com>
2023-06-21 13:34:55 -0500

    Creacion de opciones del menu

--- Menu.py
+++ Menu.py
@@ -1,2 +1,15 @@
-import math
+from os import system    
+#===========================#  Ajuste de ancho y largo de pantalla  #================================#
+system('mode con: cols=200 lines=49')
+#===========================#                Menu                   #================================#
+print("Inventario")
+print("Ventas")
+print("Proveedores")
+print("Mercancia Faltante")
+print("Estadisiticas")
+num = input("Seleccione una opcion: ")
+
+
+
+
 

2b0a2d4e947e25389c853bff0486524bbfcdd632
Sebastian David Pinzon <spinzonzambrano@gmail.com>
2023-06-22 09:21:14 -0500

    Creacion de botones

--- Menu.py
+++ Menu.py
@@ -3,7 +3,18 @@ from os import sys
 #===========================#  Ajuste de ancho y largo de pantalla  #================================#
 ventana =Tk()                           # inicializar la variable de Ventana con tkinder incluido
 ventana.geometry("720x500")             # Tamaño de la pantalla
-ventana.configure(bg='black')
+ventana.configure(bg='black')           # Color del fondo
+
+        #===========================# Botones #================================#  
+
+btn = Button(ventana,text="Ventas")
+btn.pack()
+btn1 = Button(ventana,text="Inventario")
+btn1.pack()
+btn2 = Button(ventana,text="Movimientos")
+btn2.pack()
+btn3 = Button(ventana,text="Estadisticas")
+btn3.pack()
 
 
 ventana.mainloop()
